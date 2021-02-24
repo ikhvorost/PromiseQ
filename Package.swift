bf804edaef57347aea206301ca8df8d4c7135043
Iurii Khvorost <iurii.khvorost@gmail.com>
2021-02-24 23:09:45 +0200

    swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "PromiseQ",
 	platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .macOS(.v10_10),
         .tvOS(.v9),
         .watchOS(.v2)
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "PromiseQ", dependencies: []),
-        .testTarget(name: "PromiseQTests", dependencies: ["PromiseQ"]),
+		.testTarget(name: "PromiseQTests", dependencies: ["PromiseQ"]),
     ],
 	swiftLanguageVersions: [.v5]
 )
